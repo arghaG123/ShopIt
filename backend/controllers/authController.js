44990d2b17ccac25486ac9753728b4fcabff78c4
arghaG123 <ghoshargha1989@gmail.com>
2023-01-09 23:49:39 +0530

    jwt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const errorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const ErrorHandler = require('../utils/errorHandler');
 
 exports.registerUser = catchAsyncErrors(async (req,res,next) =>{
    const {name, email, password} = req.body;
@@ -14,9 +15,37 @@ exports.registerUser = catchAsyncErrors(async (req,res,next) =>{
          url:'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819__340.png',
       }
    })
-
+   const token = user.getJwtToken();
    res.status(201).json({
       success: true,
-      user
+      token
    });
 })
+
+// Loginn user => /login
+exports.loginUser = catchAsyncErrors(async (req,res,next) =>{
+   const {email,password} = req.body;
+   //Check if email and password is entered by user
+   if(!email || !password){
+      return next(new ErrorHandler('Please enter email or password',400));
+   }
+
+   //Finding user in database
+   const user = await User.findOne({email: email}).select('+password');
+
+   if(!user){
+      return next(new ErrorHandler('Invalid email or password',401));
+   }
+
+   // Check if password is correct
+   const checkUserPassword = await user.comparePassword(password);
+
+   if(!checkUserPassword){
+      return next(new ErrorHandler('Invalid email or password',401));
+   }
+   const token = user.getJwtToken();
+   res.status(200).json({
+      success: true,
+      token
+   });
+}) 
